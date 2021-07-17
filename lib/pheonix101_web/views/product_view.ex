9a79dfaf51a374476deae99c05de5eb862a6b161
Saqib <18554574+saqe@users.noreply.github.com>
2021-07-17 14:30:38 +0500

    🐛 FIXED product HTML template view

--- lib/pheonix101_web/views/product_view.ex
+++ lib/pheonix101_web/views/product_view.ex
@@ -1,18 +1,3 @@
 defmodule Pheonix101Web.ProductView do
   use Pheonix101Web, :view
-  alias Pheonix101Web.ProductView
-
-  def render("index.json", %{products: products}) do
-    %{data: render_many(products, ProductView, "product.json")}
-  end
-
-  def render("show.json", %{product: product}) do
-    %{data: render_one(product, ProductView, "product.json")}
-  end
-
-  def render("product.json", %{product: product}) do
-    %{id: product.id,
-      title: product.title,
-      price: product.price}
-  end
 end
