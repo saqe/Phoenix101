eb33815fab1c07509852c9b7e86b1d70c8fdd20d
Saqib <18554574+saqe@users.noreply.github.com>
2021-08-04 18:26:53 +0500

    👌 IMPROVE: File renamed same as Controller & Context

--- lib/pheonix101_web/schemas/inventory.ex
+++ lib/pheonix101_web/schemas/inventory.ex
@@ -1,8 +1,9 @@
-defmodule Pheonix101Web.Schemas.Product do
+defmodule Pheonix101Web.Schemas.Inventory do
   use Absinthe.Schema.Notation
 
   object :product do
     field :id, non_null(:id)
     field :price, :float
+    field :title, :string
   end
 end
